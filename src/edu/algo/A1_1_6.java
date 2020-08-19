37c79f99ce5e6cf66773a3eb2b92892856881a8f
chesterchenn <chesterchenn@gmail.com>
2020-08-19 10:11:25 +0800

    Add A1_1_6 and A1_1_7

---
+++ src/edu/algo/A1_1_6.java
@@ -0,0 +1,17 @@
+package edu.algo;
+
+/**
+ * 下面这段程序会打印出什么？
+ */
+
+public class A1_1_6 {
+    public static void main(String[] args) {
+        int f = 0;
+        int g = 1;
+        for (int i = 0; i <= 15; i++) {
+            System.out.println(f);
+            f = f + g;
+            g = f - g;
+        }
+    }
+}
